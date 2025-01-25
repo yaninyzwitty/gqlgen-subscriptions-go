da5e2391fbc46381cdb06f64e8feb92eb014d674
yaninyzwitty <ianmwa143@gmail.com>
2025-01-25 14:45:25 +0300

    add basic tests for helper methods

---
+++ helpers/uint_to_text_test.go
@@ -0,0 +1,24 @@
+package helpers
+
+import (
+	"testing"
+)
+
+func TestUintToString(t *testing.T) {
+	tests := []struct {
+		input    uint64
+		expected string
+	}{
+		{0, "0"},
+		{1, "1"},
+		{1234567890, "1234567890"},
+		{18446744073709551615, "18446744073709551615"}, // Max uint64 value
+	}
+
+	for _, test := range tests {
+		result := UintToString(test.input)
+		if result != test.expected {
+			t.Errorf("UintToString(%d) = %s; want %s", test.input, result, test.expected)
+		}
+	}
+}
